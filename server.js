8a262eea9275e4479c2a28483b345999d70be290
Jon Bloomer <jon@freaksauce.com>
2016-04-25 22:39:20 +1000

    initial commit of basic web scraper

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+  url = 'http://www.imdb.com/title/tt2948356/?ref_=shtt_ov_tt';
+
+  request(url, function(error, response, html) {
+    if (!error) {
+      $ = cheerio.load(html);
+      var title, release, rating, duration;
+      var json = { title : "", release : "", rating : "", duration: ""};
+
+      json.title = $('.title_wrapper h1').text().replace(/\u00A0/g, '').trim();
+      json.rating = $('meta[itemprop="contentRating"]').attr('content');
+      // json.duration = $('time[itemprop="duration"]').children();
+      console.log($('time[itemprop="duration"]').children());
+      res.json(json);
+    }
+  })
+
+})
+
+app.listen('8081')
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
